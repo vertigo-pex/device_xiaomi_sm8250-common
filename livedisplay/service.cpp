9e64341c7caad33008ed5d12996577a6cbb2d9dc
RuRuTiaSaMa <1009087450@qq.com>
2022-05-26 00:38:10 +0800

    [squash] sm8250-common: Add Picture Adjustment to Livedisplay
    sm8250-common: Implement LiveDisplay HAL
    sm8250-common:livedisplay: switchto using xiaomi disp_param cmd command
    -mi dc and fod_hbm
    sm8250-common: AntiFlicker: Adapt to different nodes
    sm8250-common: livedisplay: add missing dependencies for Picture Adjustment to work
    sm8250-common: livedisplay: Fix racy init
    sm8250-common: livedisplay: remove stuff related to anti-flicker
    sm8250-common: livedisplay: Remove sinlightenhancement
    sm8250-common: livedisplay & HBM: misc. fixes
    
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Co-authored-by: Pranav-Talmale <pranavtalmale@gmail.com>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

---
+++ livedisplay/service.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019-2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.xiaomi_kona"
+
+#include <android-base/logging.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include <livedisplay/sdm/PictureAdjustment.h>
+#include <vendor/lineage/livedisplay/2.1/IPictureAdjustment.h>
+#include "livedisplay/sdm/SDMController.h"
+
+using android::OK;
+using android::sp;
+using android::status_t;
+using ::android::hardware::configureRpcThreadpool;
+using ::android::hardware::joinRpcThreadpool;
+
+using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
+using ::vendor::lineage::livedisplay::V2_1::IPictureAdjustment;
+
+int main() {
+//    status_t status = OK;
+    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+
+    LOG(INFO) << "LiveDisplay HAL service is ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "LiveDisplay HAL service failed to join thread pool.";
+    return 1;
+}
