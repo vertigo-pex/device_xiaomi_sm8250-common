4e073643f6842a1db9f000649879ee978495ac15
johnmart19 <johnivan19999@gmail.com>
2023-04-20 15:56:19 +0545

    sm8250-common: Build AOSP version of android.hardware.thermal@2.0

--- kona.mk
+++ kona.mk
@@ -527,6 +527,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
